b6a6f14c2531bf945f54d1138ed950dde5eed798
Svaraua <svarnik7@gmail.com>
2023-03-13 21:44:51 +0200

    js has been added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let num1 = prompt("Введіть перше число:");
+let num2 = prompt("Введіть друге число:");
+
+let sum = Number(num1) + Number(num2);
+let subtraction = Number(num1) - Number(num2);
+let multiplication = Number(num1) * Number(num2);
+let division = Number(num1) / Number(num2);
+
+alert(`Сума: ${sum}\nРізниця: ${subtraction}\nДобуток: ${multiplication}\nДілення: ${division}`);
