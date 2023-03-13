e42bb94366b3262af5f67cdd37bc7e5633d9f2fc
Svaraua <svarnik7@gmail.com>
2023-03-13 23:46:18 +0200

    additional conditions has been added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 let num1 = prompt("Введіть перше число:");
 let num2 = prompt("Введіть друге число:");
 
-let sum = Number(num1) + Number(num2);
-let subtraction = Number(num1) - Number(num2);
-let multiplication = Number(num1) * Number(num2);
-let division = Number(num1) / Number(num2);
+if (num1 === "" || num2 === "") {
+  alert("Помилка: обидва поля повинні бути заповнені!");
+} else {
+  num1 = Number(num1);
+  num2 = Number(num2);
 
-alert(`Сума: ${sum}\nРізниця: ${subtraction}\nДобуток: ${multiplication}\nДілення: ${division}`);
+  if (num2 === 0) {
+    alert("Помилка: Друге число не може бути 0!");
+  } else {
+    const sum = num1 + num2;
+    const subtraction = num1 - num2;
+    const multiplication = num1 * num2;
+    const division = num1 / num2;
+
+    if (num1 < num2) {
+      const confirmMsg = "Ви впевнені, що хочете продовжити операцію?";
+      if (confirm(confirmMsg)) {
+        alert(`Сума: ${sum}\nРізниця: ${subtraction}\nДобуток: ${multiplication}\nЧастка: ${division}`);
+      }
+    } else {
+      alert(`Сума: ${sum}\nРізниця: ${subtraction}\nДобуток: ${multiplication}\nЧастка: ${division}`);
+    }
+  }
+}
