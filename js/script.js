b8c6cf891d32dc33977c259f797b8d287ffd53c4
Svaraua <svarnik7@gmail.com>
2023-03-19 18:39:00 +0200

    refatored regarding lesson 24

--- js/script.js
+++ js/script.js
@@ -1,27 +1,63 @@
 let num1 = prompt("Введіть перше число:");
 let num2 = prompt("Введіть друге число:");
 
-if (num1 === "" || num2 === "") {
-  alert("Помилка: обидва поля повинні бути заповнені!");
-} else {
-  num1 = Number(num1);
-  num2 = Number(num2);
+// if (num1 === "" || num2 === "") {
+//   alert("Помилка: обидва поля повинні бути заповнені!");
+// } else {
+//   num1 = Number(num1);
+//   num2 = Number(num2);
 
-  if (num2 === 0) {
-    alert("Помилка: Друге число не може бути 0!");
-  } else {
-    const sum = num1 + num2;
-    const subtraction = num1 - num2;
-    const multiplication = num1 * num2;
-    const division = num1 / num2;
+//   if (num2 === 0) {
+//     alert("Помилка: Друге число не може бути 0!");
+//   } else {
+//     const sum = num1 + num2;
+//     const subtraction = num1 - num2;
+//     const multiplication = num1 * num2;
+//     const division = num1 / num2;
 
-    if (num1 < num2) {
-      const confirmMsg = "Ви впевнені, що хочете продовжити операцію?";
-      if (confirm(confirmMsg)) {
-        alert(`Сума: ${sum}\nРізниця: ${subtraction}\nДобуток: ${multiplication}\nЧастка: ${division}`);
-      }
-    } else {
-      alert(`Сума: ${sum}\nРізниця: ${subtraction}\nДобуток: ${multiplication}\nЧастка: ${division}`);
-    }
+//     if (num1 < num2) {
+//       const confirmMsg = "Ви впевнені, що хочете продовжити операцію?";
+//       if (confirm(confirmMsg)) {
+//         alert(`Сума: ${sum}\nРізниця: ${subtraction}\nДобуток: ${multiplication}\nЧастка: ${division}`);
+//       }
+//     } else {
+//       alert(`Сума: ${sum}\nРізниця: ${subtraction}\nДобуток: ${multiplication}\nЧастка: ${division}`);
+//     }
+//   }
+// }
+
+function addNumbers(num1, num2) {
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return "Параметри повинні бути числами!";
   }
+  return num1 + num2;
 }
+
+function subtractNumbers(num1, num2) {
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return "Параметри повинні бути числами!";
+  }
+  return num1 - num2;
+}
+
+function multiplyNumbers(num1, num2) {
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return "Параметри повинні бути числами!";
+  }
+  return num1 * num2;
+}
+
+function divideNumbers(num1, num2) {
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return "Параметри повинні бути числами!";
+  }
+  if (num2 === 0) {
+    return "Ділення на нуль неможливе!";
+  }
+  return num1 / num2;
+}
+
+console.log("Сума:" + addNumbers(+num1, +num2));
+console.log("Різниця:" + subtractNumbers(+num1, +num2));
+console.log("Добуток:" + multiplyNumbers(+num1, +num2));
+console.log("Частка:" + divideNumbers(+num1, +num2));
---
+++ js/script2.js
@@ -0,0 +1,55 @@
+const arr = [7, 9, 10, 13, 18, 23, 27, 31, 33, 43];
+
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (typeof arr[i] === 'number') {
+//     sum += arr[i];
+//   }
+// }
+
+// console.log('Сума елементів масиву:', sum);
+
+// let min = arr[0];
+// let max = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//   if (typeof arr[i] === 'number') {
+//     if (arr[i] < min) {
+//       min = arr[i];
+//     }
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+// }
+
+// console.log('Мінімальне значення елементу масиву:', min);
+// console.log('Максимальне значення елементу масиву:', max);
+
+// let symbols = '';
+// for (let i = 1; i <= 5; i++) {
+//   symbols += '#';
+//   console.log(symbols);
+// }
+
+function findMin(arr) {
+  let min = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return min;
+}
+
+function findMax(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+console.log("Мінімальне число у масиві: " + findMin(arr));
+console.log("Максимальне число у масиві: " + findMax(arr));
